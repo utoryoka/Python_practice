524402e1f80053797cb9481a4a41aa85ab80ae69
DESKTOP-768BM4M\kfg_admin <uto.ryoka@kyushu-fg.co.jp>
2022-08-09 14:31:34 +0900

    20220809 python一年生終了

---
+++ Python_1/chino.py
@@ -0,0 +1,58 @@
+#画像を読み込んで数字を判定する。（数字の太さが大きくないと精度が悪い）
+import tkinter as tk
+import tkinter.filedialog as fd
+import PIL.Image
+import PIL.ImageTk
+
+import sklearn.datasets
+import sklearn.svm
+import numpy
+
+#画像ファイルを数値リストに変換する
+def imageToData(filename):
+    #画像を8x8のグレースケールに変換
+    grayImage = PIL.Image.open(filename).convert("L")
+    grayImage = grayImage.resize((8,8),PIL.Image.ANTIALIAS)
+    #画像を表示する
+    dispImage = PIL.ImageTk.PhotoImage(grayImage.resize((300,300)))
+    imageLabel.configure(image = dispImage)
+    imageLabel.image = dispImage
+    #数値リストに変換
+    numImage = numpy.asarray(grayImage, dtype = float)
+    numImage = numpy.floor(16-16*(numImage/256))
+    numImage = numImage.flatten()
+    return numImage
+
+#数字を予測する
+def predictDigits(data):
+    #学習用データを読み込む
+    digits = sklearn.datasets.load_digits()
+    #機械学習する
+    clf =sklearn.svm.SVC(gamma=0.001)
+    clf.fit(digits.data, digits.target)
+    #予測結果を表示する
+    n= clf.predict([data])
+    textLabel.configure(text = "この画像は"+str(n)+"です！")    
+
+def openFile():
+    fpath = fd.askopenfilename()
+    
+    if fpath:
+        #画像ファイルを数値リストに変換する
+        data = imageToData(fpath)
+        #数値を予測する
+        predictDigits(data)
+
+root =tk.Tk()
+root.geometry("400x400")
+
+btn = tk.Button(text= "ファイルを開く",command = openFile)
+imageLabel = tk.Label()
+btn.pack()
+imageLabel.pack()
+#予測結果を表示するラベル
+textLabel = tk.Label(text="手書きの数字を認識します！")
+textLabel.pack()
+
+tk.mainloop()
+
